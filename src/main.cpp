9d98b9f1c2f923c862c3d93189474e51f12ed219
Juan Carlos Aitajunki <aitajunki@gmail.com>
2018-04-13 20:08:49 +0200

    Version funcional del programa. No incluye Sleep.

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,47 @@
 /*
-   MQTT Sensor - Temperature, Humidity and Pressure (BME280) for Home-Assistant - NodeMCU (ESP8266)
-   https://home-assistant.io/components/sensor.mqtt/
+   MQTT Sensor - Temperature, Humidity (DHT22) and Temperature, Pressure (BMP085)
+   for Home-Assistant - NodeMCU (ESP8266)
 
    Libraries :
     - ESP8266 core for Arduino : https://github.com/esp8266/Arduino
     - PubSubClient : https://github.com/knolleary/pubsubclient
-    - BME280 : https://github.com/adafruit/Adafruit_BME280_Library
     - ArduinoJson : https://github.com/bblanchon/ArduinoJson
-
-   Sources :
-    - File > Examples > ES8266WiFi > WiFiClient
-    - File > Examples > PubSubClient > mqtt_auth
-    - File > Examples > PubSubClient > mqtt_esp8266
-    - File > Examples > BME280 sensor library > bme280test
-    - File > Examples > ArduinoJson > JsonGeneratorExample
 
    Configuration (HA) :
     sensor 1:
       platform: mqtt
-      state_topic: 'office/bme280'
-      name: 'Temperature'
+      state_topic: 'weatherstation/sensor'
+      name: 'Temperature DHT22'
       unit_of_measurement: '°C'
-      value_template: '{{ value_json.temperature }}'
+      value_template: '{{ value_json.dht_temperature }}'
 
     sensor 2:
       platform: mqtt
-      state_topic: 'office/bme280'
-      name: 'Humidity'
+      state_topic: 'weatherstation/sensor'
+      name: 'Humidity DHT22'
       unit_of_measurement: '%'
-      value_template: '{{ value_json.humidity }}'
+      value_template: '{{ value_json.dht_humidity }}'
 
     sensor 3:
       platform: mqtt
-      state_topic: 'office/bme280'
-      name: 'Pressure'
+      state_topic: 'weatherstation/sensor'
+      name: 'Temperature BMP085'
       unit_of_measurement: 'mBar'
-      value_template: '{{ value_json.pressure }}'
+      value_template: '{{ value_json.bmp_temperature }}'
+
+    sensor 4:
+      platform: mqtt
+      state_topic: 'weatherstation/sensor'
+      name: 'Pressure BMP085'
+      unit_of_measurement: 'mBar'
+      value_template: '{{ value_json.bmp_pressure }}'
 
    Samuel M. - v1.1 - 08.2016
    If you like this example, please add a star! Thank you!
    https://github.com/mertenats/open-home-automation
 */
 
+#include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <DHT.h>
@@ -56,17 +56,17 @@ const char* WIFI_SSID = "HomeAssistantMQTT";
 const char* WIFI_PASSWORD = "junkilin";
 
 // MQTT: ID, server IP, port, username and password
-const PROGMEM char* MQTT_CLIENT_ID = "terraza";
+const PROGMEM char* MQTT_CLIENT_ID = "weatherstation";
 const PROGMEM char* MQTT_SERVER_IP = "192.168.0.104";
 const PROGMEM uint16_t MQTT_SERVER_PORT = 1883;
 const PROGMEM char* MQTT_USER = "homeassistant";
 const PROGMEM char* MQTT_PASSWORD = "raspberry";
 
 // MQTT: topic
-const PROGMEM char* MQTT_SENSOR_TOPIC = "terraza/sensor";
+const PROGMEM char* MQTT_SENSOR_TOPIC = "weatherstation/sensor";
 
 // DHT 22
-#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+#define DHTTYPE DHT22   // DHT22  (AM2302), AM2321
 #define DHTPIN D1       // what digital pin we're connected to
 
 // sleeping time
@@ -78,22 +78,24 @@ DHT dht(DHTPIN, DHTTYPE);
 Adafruit_BMP085 bmp;
 
 // function called to publish the temperature and the humidity
-void publishData(float p_temperature, float p_humidity, float p_pressure) {
+void publishData(float p_t_dht, float p_h_dht, float p_t_bmp, float p_p_bmp) {
   // create a JSON object
   // doc : https://github.com/bblanchon/ArduinoJson/wiki/API%20Reference
   StaticJsonBuffer<200> jsonBuffer;
   JsonObject& root = jsonBuffer.createObject();
   // INFO: the data must be converted into a string; a problem occurs when using floats...
-  root["temperature"] = (String)p_temperature;
-  root["humidity"] = (String)p_humidity;
-  root["pressure"] = (String)p_pressure;
+  root["dht_temperature"] = (String)p_t_dht;
+  root["dht_humidity"] = (String)p_h_dht;
+  root["bmp_temperature"] = (String)p_t_bmp;
+  root["bmp_pressure"] = (String)p_p_bmp;
   root.prettyPrintTo(Serial);
   Serial.println("");
   /*
      {
-        "temperature": "23.20",
-        "humidity": "43.70",
-		    "pressure": "934.81"
+        "dht_temperature": "23.20",
+        "dht_humidity": "43.70",
+        "bmp_temperature": "22.49",
+		    "bmp_pressure": "934.81"
      }
   */
   char data[200];
@@ -115,7 +117,7 @@ void reconnect() {
     } else {
       Serial.print("ERROR: failed, rc=");
       Serial.print(client.state());
-      Serial.println("DEBUG: try again in 5 seconds");
+      Serial.println(" DEBUG: try again in 5 seconds");
       // Wait 5 seconds before retrying
       delay(5000);
     }
@@ -125,6 +127,8 @@ void reconnect() {
 void setup() {
   // init the serial
   Serial.begin(115200);
+
+  pinMode(D5, OUTPUT); // function blickLED
 
   dht.begin();
   Wire.begin(D2, D3); // sda, scl
@@ -139,7 +143,7 @@ void setup() {
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
   // Fixed IP
-  IPAddress IP(192,168,0,204);
+  IPAddress IP(192,168,0,205);
   IPAddress GATEWAY(192,168,0,1);
   IPAddress SUBNET(255,255,255,0);
   WiFi.config(IP, GATEWAY, SUBNET);
@@ -159,6 +163,14 @@ void setup() {
   client.setCallback(callback);
 }
 
+// function blickLED
+void blickLED(){
+  digitalWrite(D5, HIGH);
+  delay(100);
+  digitalWrite(D5, LOW);
+  delay(100);
+}
+
 void loop() {
   if (!client.connected()) {
     reconnect();
@@ -168,22 +180,28 @@ void loop() {
   // Reading temperature, humidity or pressure takes about 250 milliseconds!
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
   // Read temperature as Celsius (the default)
-  float t = dht.readTemperature()-2;
+  float t_dht = dht.readTemperature()-2;
   // Read humidity as %
-  float h = dht.readHumidity();
+  float h_dht = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t_bmp = bmp.readTemperature()-2;
   // Read pressure en mBar
-  float p = bmp.readPressure()/100;
+  float p_bmp = bmp.readPressure()/100.00;
 
-  if (isnan(h) || isnan(t)) {
-    Serial.println("ERROR: Failed to read from DHT sensor!");
+  if (isnan(h_dht) || isnan(t_dht) || isnan(t_bmp) || isnan(p_bmp)) {
+    Serial.println("ERROR: Failed to read from DHT22 or BMP085 sensor!");
     return;
   } else {
-    //Serial.println(t);
-    //Serial.println(h);
-	  //Serial.println(p);
-    publishData(t, h, p);
+    // Serial.println(t_dht);
+    // Serial.println(h_dht);
+	  // Serial.println(t_bmp);
+    // Serial.println(p_bmp);
+    publishData(t_dht, h_dht, t_bmp, p_bmp);
   }
 
+  blickLED ();
+
+  // delay (4000);
   delay(600*1000);
 
   // Serial.println("INFO: Closing the MQTT connection");
